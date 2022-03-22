0f67c6781349b2f3a5f322b5c9ff4dba2aeca18b
xkazxx <xkazxx@qq.com>
2022-03-22 17:43:28 +0800

    单例模式

---
+++ src/main/java/com/xkazxx/designpattern/createBeanMode/singletonPattern/HungryLoad.java
@@ -0,0 +1,7 @@
+package com.xkazxx.designpattern.createBeanMode.singletonPattern;/**
+ * @author created by xkazxx
+ * @version v0.0.1
+ * description: com.xkazxx.designpattern.createBeanMode.singletonPattern
+ * date:2022/3/22
+ */public class HungryLoad {
+}
